0349f55cc932b7a6cf2755bd598a9c5a87a315ed
liveforone <yc4852@gmail.com>
2022-03-03 14:20:35 +0900

    Good commit

--- src/main/java/jpabook/japshop/controller/MemberController.java
+++ src/main/java/jpabook/japshop/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -39,5 +40,12 @@ public class MemberController {
 
         memberService.join(member);  //저장
         return "redirect:/";  //홈으로 넘어가기
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
